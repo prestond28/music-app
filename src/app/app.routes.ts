e75ec0d10ccc661b00ff6a81ab92fa3dce94b239
prestond28 <pdavis2897@gmail.com>
2025-06-02 23:02:28 -0700

    Added game component and updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'game/:genre',
+        component: GameComponent
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+    }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  imports: [],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss'
+})
+
+export class GameComponent implements OnInit {
+
+  ngOnInit(): void {
+      
+  }
+}
